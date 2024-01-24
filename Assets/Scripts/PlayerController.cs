24dadb68c8d3f6d5a4902158e62c5f8ebdedb2f2
richardzz15 <richardz0520@gmail.com>
2024-01-24 12:55:05 -0800

    jump fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float JumpHeight = 5f;
 
     Vector3 MoveValue = Vector3.zero;
+    bool isGrounded = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -26,7 +28,10 @@ public class PlayerController : MonoBehaviour
 
     void OnJump()
     {
-        Jump();
+        if (isGrounded)
+        {
+            Jump();
+        }
     }
 
     private void Jump()
@@ -34,25 +39,52 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector3(rb.velocity.x, JumpHeight, rb.velocity.z);
     }
 
-    void OnMove(InputValue value)
+    private void OnCollisionEnter(Collision collision)
     {
-        Vector2 direction = value.Get<Vector2>();
-        Debug.Log(direction);
-        MoveValue = direction;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
     }
 
-    private void Move(float x, float z)
+    private void OnCollisionExit(Collision collision)
     {
-        rb.velocity = new Vector3(x * speed, rb.velocity.y, z * speed);
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 
-    private void Flatten()
+    private void OnCollisionStay(Collision collision)
     {
-        transform.localScale = new Vector3(2f, 0.5f, 2f);
-    }
+        Vector3 norm = collision.GetContact(0).normal;
 
-    void OnFlatten()
-    {
-        Flatten();
+        if (Vector3.Angle(norm, Vector3.up) < 45)
+        {
+            isGrounded = true;
+        }
     }
+        void OnMove(InputValue value)
+        {
+            Vector2 direction = value.Get<Vector2>();
+            Debug.Log(direction);
+            MoveValue = direction;
+        }
+
+        private void Move(float x, float z)
+        {
+            rb.velocity = new Vector3(x * speed, rb.velocity.y, z * speed);
+        }
+
+        private void Flatten()
+        {
+            transform.localScale = new Vector3(2f, 0.5f, 2f);
+        }
+
+        void OnFlatten()
+        {
+            Flatten();
+        }
+
 }
